c10b7b7a3f9caa1b244cde36f383c9deb1ae81bf
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-21 14:20:20 -0700

    Used dispatch with useActions hook for RepositoriesList

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,23 +1,24 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
-  const [term, setTerm] = useState('');
-  const dispatch = useDispatch();
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    dispatch(actionCreators.searchRepositories(term) as any);
+    searchRepositories(term);
   };
 
-  return <div>
-    <form onSubmit={onSubmit} >
-      <input value={term} onChange={e => setTerm(e.target.value)} />
-      <button>Search</button>
-    </form>
-  </div>
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
 };
 
-export default RepositoriesList
+export default RepositoriesList;
